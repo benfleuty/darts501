06e3f1d1b0c45a42cf1f2e4d28e0a4e250d9789a
Ben Fleuty <22148138+benfleuty@users.noreply.github.com>
2020-04-18 03:00:57 +0100

    Tidied - ctime now being used for rand seed

--- main.cpp
+++ main.cpp
@@ -1,13 +1,12 @@
 #include <iostream>
 #include <string>
 #include <climits> // INT_MAX
-#include <math.h>
+#include <cmath>
+#include <ctime>
 
 #include "UserIO.h"
 #include "Game.h"
 #include "Player.h"
-
-#define GET_SQUARE = pow(int,2)
 
 typedef unsigned short ushort;
 typedef unsigned int uint;
@@ -23,11 +22,10 @@ void SetupP1() {
     UserIO::ClearScreen();
     do {
         P1.SetAccuracy(UserIO::uShort("Enter " + P1.GetName() + "'s accuracy [1-100]: "));
-        if(P1.GetAccuracy() > 0 && P1.GetAccuracy() <= 100) break;
+        if (P1.GetAccuracy() > 0 && P1.GetAccuracy() <= 100) break;
         cout << "\nInvalid accuracy!";
         UserIO::ClearScreen();
-    }
-    while (P1.GetAccuracy() == 0 || P1.GetAccuracy() > 100);
+    } while (P1.GetAccuracy() == 0 || P1.GetAccuracy() > 100);
     P1.SetCPU(UserIO::Bool("Is " + P1.GetName() + " controlled by the computer? [Y/N]: "));
 }
 
@@ -36,11 +34,10 @@ void SetupP2() {
     UserIO::ClearScreen();
     do {
         P2.SetAccuracy(UserIO::uShort("Enter " + P2.GetName() + "'s accuracy [1-100]: "));
-        if(P2.GetAccuracy() > 0 && P2.GetAccuracy() <= 100) break;
+        if (P2.GetAccuracy() > 0 && P2.GetAccuracy() <= 100) break;
         cout << "\nInvalid accuracy!";
         UserIO::ClearScreen();
-    }
-    while (P2.GetAccuracy() == 0 || P2.GetAccuracy() > 100);
+    } while (P2.GetAccuracy() == 0 || P2.GetAccuracy() > 100);
     P2.SetCPU(UserIO::Bool("Is " + P2.GetName() + " controlled by the computer? [Y/N]: "));
 }
 
@@ -48,9 +45,9 @@ bool CheckP1() {
     while (true) {
         UserIO::ClearScreen();
         string prompt = "Player 1:\nName: " + P1.GetName() +
-                "\nAccuracy: " + to_string(P1.GetAccuracy()) +
-                "\n" + ((P1.IsCPU()) ? "CPU" : "User" ) + " controlled"+
-                "\nAre these details correct? [Y/N]:";
+                        "\nAccuracy: " + to_string(P1.GetAccuracy()) +
+                        "\n" + ((P1.IsCPU()) ? "CPU" : "User") + " controlled" +
+                        "\nAre these details correct? [Y/N]:";
         return UserIO::Bool(prompt);
 
     }
@@ -60,16 +57,15 @@ bool CheckP2() {
     while (true) {
         UserIO::ClearScreen();
         string prompt = "Player 2:\nName: " + P2.GetName() +
-                "\nAccuracy: " + to_string(P2.GetAccuracy()) +
-                 "\n" + ((P2.IsCPU()) ? "CPU" : "User" ) + " controlled"+
-                "\nAre these details correct? [Y/N]:";
+                        "\nAccuracy: " + to_string(P2.GetAccuracy()) +
+                        "\n" + ((P2.IsCPU()) ? "CPU" : "User") + " controlled" +
+                        "\nAre these details correct? [Y/N]:";
         return UserIO::Bool(prompt);
     }
 }
 
 void CheckPlayers() {
-    while (true)
-    {
+    while (true) {
         bool P1Ready = CheckP1();
         UserIO::ClearScreen();
         if (!P1Ready) SetupP1();
@@ -83,45 +79,45 @@ void CheckPlayers() {
 }
 
 
-ushort CalcThrowResult(ushort playerAccuracy){
+ushort CalcThrowResult(ushort playerAccuracy) {
     ushort accuracy = playerAccuracy;
     ushort randNum = rand() % 101;
 
     // hit
-    if(randNum < accuracy) return 0;
+    if (randNum < accuracy) return 0;
 
     // miss
     return abs(randNum - accuracy);
     // range between randNum and accuracy is the distance from the bull
 }
 
-float sq(float value){
-    return pow(value,2);
+float sq(float value) {
+    return pow(value, 2);
 }
 
-float getHyp(float x,float y, bool SQRT = true){
+float getHyp(float x, float y, bool SQRT = true) {
     if (SQRT) return sqrt(sq(x) + sq(y));
     return sq(x) + sq(y);
 }
 
 bool WhoThrowsFirst() {
-    ushort p1X,p2X,p1Y,p2Y;
-    float p1Result,p2Result;
-    while(true){
+    ushort p1X, p2X, p1Y, p2Y;
+    float p1Result, p2Result;
+    while (true) {
         // halve results for midpoint of hyp
         p1X = CalcThrowResult(P1.GetAccuracy()) * 0.5;
         p1Y = CalcThrowResult(P1.GetAccuracy()) * 0.5;
 
         // get the length of the midpoint
-        p1Result = getHyp(p1X,p1Y);
+        p1Result = getHyp(p1X, p1Y);
 
         p2X = CalcThrowResult(P2.GetAccuracy()) * 0.5;
         p2Y = CalcThrowResult(P2.GetAccuracy()) * 0.5;
 
-        p2Result = getHyp(p2X,p2Y);
+        p2Result = getHyp(p2X, p2Y);
 
         if (p1Result < p2Result) return true;
-        else if(p2Result < p1Result) return false;
+        else if (p2Result < p1Result) return false;
     }
 }
 //endregion
@@ -131,7 +127,8 @@ bool WhoThrowsFirst() {
 uint GetIterations() {
     uint simulationIterations = 0;
     while (!simulationIterations) {
-        string prompt = "\nEnter the number of iterations you want to run\n[Min: 1, Max: " + to_string(UINT_MAX) + "]: ";
+        string prompt =
+                "\nEnter the number of iterations you want to run\n[Min: 1, Max: " + to_string(UINT_MAX) + "]: ";
         simulationIterations = UserIO::uInt(prompt);
     }
     return simulationIterations;
@@ -143,8 +140,7 @@ Game SetupGame() {
     if (firstThrower) {
         msg = P1.GetName();
         cout << P1.GetName();
-    }
-    else {
+    } else {
         msg = P2.GetName();
         cout << P2.GetName();
     }
@@ -157,7 +153,7 @@ Game SetupGame() {
 //endregion
 
 int main() {
-    srand(ulong(0)); // cast to ulong to stop warning C4244
+    srand(time(0));
     UserIO::ClearScreen();
 #ifdef _WIN32
     system("title \"501 Darts | Ben Fleuty 1900040 | CMP 102 Assessment");
@@ -181,7 +177,9 @@ int main() {
     game.Start();
 #else
     UserIO::ClearScreen();
-    enum Player {User,CPU};
+    enum Player {
+        User, CPU
+    };
     P1.SetName("ben");
     P1.SetAccuracy(73);
     P1.SetCPU(CPU);
@@ -190,7 +188,7 @@ int main() {
     P2.SetAccuracy(71);
     P2.SetCPU(CPU);
 
-    Game game = Game(P1,P2,10000,0);
+    Game game = Game(P1, P2, 100, 0);
     game.Start();
 
 #endif
